4bd95ae3cddfe27962319c434a266451c543ec33
sangmin9412 <tkdals9048@naver.com>
2025-08-21 01:20:06 +0900

    update: prettierrc, eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,38 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // 상대 경로 사용 금지
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "no-restricted-imports": [
+        "warn",
+        {
+          patterns: [
+            {
+              group: ["../*"],
+              message: "Consider using absolute imports instead of relative parent imports"
+            }
+          ]
+        }
+      ]
+    },
+    settings: {
+      "import/parsers": {
+        "@typescript-eslint/parser": [".ts", ".tsx"]
+      },
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true
+        }
+      }
+    }
+  }
 ];
 
 export default eslintConfig;
