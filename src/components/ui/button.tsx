48a16723e87d814690abe2d0e3257cf65ff9efc3
sangmin9412 <tkdals9048@naver.com>
2025-08-26 02:57:05 +0900

    feat: dot-list 공통 컴포넌트 추가 button 기본 스타일 반영

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,39 +1,36 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-[1.8rem] leading-[3rem] font-bold disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer transition-[transform,background-color,scale] duration-200 ease-in-out active:scale-90 focus-visible:scale-90",
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
-          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+          "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
-        ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
+          "border border-black bg-background hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+        secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+        link: "text-primary underline-offset-4 hover:underline"
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9",
-      },
+        default: "h-[6.2rem] px-[1.6rem] py-[1.6rem] has-[>svg]:px-[1.6rem]",
+        sm: "h-[4.8rem] gap-[1.2rem] px-[1.6rem] has-[>svg]:px-[1.2rem]",
+        lg: "h-[7.2rem] px-[1.6rem] py-[1.6rem] text-[2rem] leading-[3.2rem] has-[>svg]:px-[1.6rem]",
+        icon: "size-9"
+      }
     },
     defaultVariants: {
       variant: "default",
-      size: "default",
-    },
+      size: "default"
+    }
   }
-)
+);
 
 function Button({
   className,
@@ -43,17 +40,11 @@ function Button({
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
+    asChild?: boolean;
   }) {
-  const Comp = asChild ? Slot : "button"
+  const Comp = asChild ? Slot : "button";
 
-  return (
-    <Comp
-      data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
-    />
-  )
+  return <Comp data-slot='button' className={cn(buttonVariants({ variant, size, className }))} {...props} />;
 }
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
---
+++ src/components/ui/dot-list.tsx
@@ -0,0 +1,36 @@
+import { cn } from "@/lib/utils";
+import { Slot } from "@radix-ui/react-slot";
+
+type DotListProps = {
+  className?: string;
+  children: React.ReactNode;
+};
+
+type DotItemProps = React.ComponentProps<"li"> & {
+  asChild?: boolean;
+  className?: string;
+  children: React.ReactNode;
+};
+
+const DotList = ({ className, children }: DotListProps) => {
+  return <ul className={cn("flex flex-col gap-[.8rem]", className)}>{children}</ul>;
+};
+
+const DotItem = ({ asChild, className, children, ...props }: DotItemProps) => {
+  const Comp = asChild ? Slot : "li";
+
+  return (
+    <Comp
+      className={cn(
+        "relative flex flex-col pl-[1.6rem] text-[1.8rem] leading-[3rem] [--line-height:3rem] [--dot-size:0.4rem]",
+        className
+      )}
+      {...props}
+    >
+      <span className='absolute left-0 top-0 flex items-center h-[var(--line-height)] before:size-[var(--dot-size)] before:rounded-full before:bg-[currentColor]'></span>
+      {children}
+    </Comp>
+  );
+};
+
+export { DotList, DotItem };
