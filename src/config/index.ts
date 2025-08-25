a183bf00232b1c1fae1efbd0cf503c7c5f8cc5df
sangmin9412 <tkdals9048@naver.com>
2025-08-26 02:56:33 +0900

    feat: 이벤트 기간 상수 설정

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,13 @@
 export const basePath = "";
+export const EVENT_INFO = {
+  info: {
+    start: "2025. 09. 29 (Mon)",
+    end: "2025. 11. 10 (Mon)",
+    announcement: "2025. 11. 00 (Mon)"
+  },
+  date: {
+    start: "2025-09-29",
+    end: "2025-11-10",
+    announcement: "2025-11-00"
+  }
+};
