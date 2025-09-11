7f761f4d907c3ff982eb9e2139600737f099a3c4
sangmin9412 <tkdals9048@naver.com>
2025-09-11 14:11:21 +0900

    fix: loading 크기 수정

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -3,7 +3,7 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export const Loading = ({ className }: { className?: string }) => {
   return (
-    <div className={cn("mx-auto desktop:max-w-[28rem] flex items-center justify-center", className)}>
+    <div className={cn("mx-auto tablet:max-w-[28rem] flex items-center justify-center", className)}>
       <DotLottieReact src={"/lotties/loading/Insider-loading.json"} loop autoplay />
     </div>
   );
