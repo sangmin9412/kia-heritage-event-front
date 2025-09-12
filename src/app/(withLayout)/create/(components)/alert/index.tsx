350be26054556a6e06944e379c183231b755f591
sangmin9412 <tkdals9048@naver.com>
2025-09-12 16:01:19 +0900

    fix: create create/submit 접근제한 임시 주석

--- src/app/(withLayout)/create/(components)/alert/index.tsx
+++ src/app/(withLayout)/create/(components)/alert/index.tsx
@@ -47,40 +47,40 @@ export const CreateAlert = () => {
     // 이벤트 참여 정보가 없으면 알림 띄우기
     const isEventEnterForm = Object.keys(userForm).length > 0;
     if (!isEventEnterForm && isCreateFormPage) {
-      open({
-        title: "이벤트 참여 정보가 없습니다.",
-        description: "메인 페이지로 이동하여<br /> 이벤트 참여 정보를 입력해주세요.",
-        onConfirm() {
-          router.push(ROUTES.HOME.link);
-        },
-        onClose() {
-          router.push(ROUTES.HOME.link);
-        }
-      });
+      // open({
+      //   title: "이벤트 참여 정보가 없습니다.",
+      //   description: "메인 페이지로 이동하여<br /> 이벤트 참여 정보를 입력해주세요.",
+      //   onConfirm() {
+      //     router.push(ROUTES.HOME.link);
+      //   },
+      //   onClose() {
+      //     router.push(ROUTES.HOME.link);
+      //   }
+      // });
       return;
     }
 
     // 이벤트 참여 정보가 있으면 포스터 제작 페이지로 이동
     const isPosterForm = posterForm.carCode && posterForm.title && posterForm.instagramId && posterForm.imageBase64;
     if (!isPosterForm && isCreateSubmitPage) {
-      open({
-        title: "포스터 제작 정보가 없습니다.",
-        description: "포스터 제작 정보를 입력해주세요.",
-        onConfirm() {
-          if (!isEventEnterForm) {
-            router.push(ROUTES.HOME.link);
-          } else {
-            router.push(ROUTES.CREATE_FORM.link);
-          }
-        },
-        onClose() {
-          if (!isEventEnterForm) {
-            router.push(ROUTES.HOME.link);
-          } else {
-            router.push(ROUTES.CREATE_FORM.link);
-          }
-        }
-      });
+      // open({
+      //   title: "포스터 제작 정보가 없습니다.",
+      //   description: "포스터 제작 정보를 입력해주세요.",
+      //   onConfirm() {
+      //     if (!isEventEnterForm) {
+      //       router.push(ROUTES.HOME.link);
+      //     } else {
+      //       router.push(ROUTES.CREATE_FORM.link);
+      //     }
+      //   },
+      //   onClose() {
+      //     if (!isEventEnterForm) {
+      //       router.push(ROUTES.HOME.link);
+      //     } else {
+      //       router.push(ROUTES.CREATE_FORM.link);
+      //     }
+      //   }
+      // });
       return;
     }
   }, [router, open, userForm, posterForm, hasHydrated, isCreateFormPage, isCreateSubmitPage]);
