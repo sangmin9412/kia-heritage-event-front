fa332da797d8dfd9bf29f6bcbc187fea92d00710
sangmin9412 <tkdals9048@naver.com>
2025-09-24 01:58:09 +0900

    fix: 주석 삭제 오타 수정

--- src/components/ui/form/form-field-input.tsx
+++ src/components/ui/form/form-field-input.tsx
@@ -27,10 +27,6 @@ export function FormFieldInput<T extends FieldValues>({
         render={({ field, fieldState }) => {
           const { error } = fieldState;
 
-          if (name === "oldKiaCar") {
-            console.log("fieldState", error);
-          }
-
           return (
             <FormItem>
               {label && <FormLabel>{label}</FormLabel>}
--- src/features/poster/event-enter-form/components/form/index.tsx
+++ src/features/poster/event-enter-form/components/form/index.tsx
@@ -254,7 +254,7 @@ export const EventEnterForm = () => {
             <FormRow
               label='현재 기아 차량을 보유하고 있다면, 어떤 차종입니까?'
               names={["currentKiaCar"]}
-              className={cn("relative", form.watch("isCurrentKiaCar") !== "false" && "hidden")}
+              className={cn("relative", form.watch("isCurrentKiaCar") !== "true" && "hidden")}
             >
               <FormFieldInput
                 form={form}
@@ -280,7 +280,7 @@ export const EventEnterForm = () => {
             <FormRow
               label='현재 보유하고 있는 차종은 어떤 차종입니까?'
               names={["currentNonKiaCar"]}
-              className={cn("relative", form.watch("isCurrentKiaCar") !== "true" && "hidden")}
+              className={cn("relative", form.watch("isCurrentKiaCar") !== "false" && "hidden")}
             >
               <FormFieldInput
                 form={form}
