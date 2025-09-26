e98fa120bad9456aa5414d938bcc065e99b57cde
Sangmin1212 <tkdals9048@imform.co.kr>
2025-09-26 17:46:32 +0900

    fix: 미들웨어 페이지 접근 제한 롤백 - 수정 필요

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,14 +7,14 @@ export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   
   // 한국시간 2025년 9월 29일 08:00:00
-  const eventStartDateTime = EVENT_START_DATE_TIME;
-  const currentDateTime = dayjs().format("YYYY-MM-DD HH:mm:ss");
-  const isEventActive = isAfterDate(eventStartDateTime, currentDateTime);
+  // const eventStartDateTime = EVENT_START_DATE_TIME;
+  // const currentDateTime = dayjs().format("YYYY-MM-DD HH:mm:ss");
+  // const isEventActive = isAfterDate(eventStartDateTime, currentDateTime);
   
   // 이벤트 시작 전이고, 메인 페이지(/)가 아닌 경우 메인 페이지로 리다이렉트
-  if (!isEventActive && pathname !== "/") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  // if (!isEventActive && pathname !== "/") {
+  //   return NextResponse.redirect(new URL("/", request.url));
+  // }
 
   const response = NextResponse.next();
   return response;
